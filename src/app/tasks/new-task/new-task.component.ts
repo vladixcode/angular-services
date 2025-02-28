6e8e9f6956e1fecd2cb89a73a75cdd5c312f94cd
Vladimir Ilic <vladimir.ilic101@gmail.com>
2025-02-28 15:41:17 +0100

    Injecting a Service into Component using Angular Dependency Injection with a constructor method

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, viewChild } from '@angular/core'
 import { FormsModule } from '@angular/forms'
+import { TasksService } from '../tasks.service'
 
 @Component({
   selector: 'app-new-task',
@@ -11,8 +12,18 @@ import { FormsModule } from '@angular/forms'
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form')
 
+  // private tasksService: TasksService
+
+  // constructor(tService: TasksService) {
+  //   this.tasksService = tService
+  // }
+
+  // TypeScript shortcut
+  constructor(private tasksService: TasksService) {}
+
   onAddTask(title: string, description: string) {
     console.log(title, description)
+    this.tasksService.addTask({ title, description })
     this.formEl()?.nativeElement.reset()
   }
 }
